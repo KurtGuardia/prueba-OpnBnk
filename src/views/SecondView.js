a6509096f6c41b3575be796a3a6ef6125cc09bca
Kurt Guardia <kurtguardia@gmail.com>
2021-04-15 20:27:57 +0200

    implemented password requirements check

--- src/views/SecondView.js
+++ src/views/SecondView.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Steps from '../components/Steps';
 import Button from '../components/Button';
+import { useHistory } from 'react-router-dom';
+
 const SecondView = () => {
+  const [password, setPassword] = useState('');
+  const [clueCount, setClueCount] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+  const [isCorrectLength, setIsCorrectLength] = useState(false);
+  const [hasANumber, setHasANumber] = useState(false);
+  const [hasCapitalLetter, setHasCapitalLetter] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (isCorrectLength && hasANumber && hasCapitalLetter) setIsValid(true);
+    if (!isCorrectLength || !hasANumber || !hasCapitalLetter) setIsValid(false);
+  }, [isCorrectLength, hasANumber, hasCapitalLetter]);
+
+  const handleMainPasswordInput = (e) => {
+    e.preventDefault();
+    const input = e.target.value;
+    const numberPattern = /\d/;
+    const capitalPattern = /(?=.*[A-Z])/;
+
+    setPassword(input);
+
+    if (input.length > 7) setIsCorrectLength(true);
+    if (input.length > 24) setIsCorrectLength(false);
+    if (input.length < 7) setIsCorrectLength(false);
+
+    if (numberPattern.test(input)) setHasANumber(true);
+    if (!numberPattern.test(input)) setHasANumber(false);
+
+    if (capitalPattern.test(input)) setHasCapitalLetter(true);
+    if (!capitalPattern.test(input)) setHasCapitalLetter(false);
+  };
+
+  const handleRepeatPassword = (e) => {
+    e.preventDefault();
+    const input = e.target.value;
+
+    if (input === password) {
+      setPasswordsMatch(true);
+      setDisabled(false);
+    }
+  };
+
+  const handleSubmit = () => {
+    history.push('/3');
+    console.log('hey');
+  };
+
   return (
     <div className='SecondView'>
       <Steps />{' '}
@@ -19,49 +70,73 @@ const SecondView = () => {
         </p>
         <div className='SecondView__content--passwords'>
           <div className='main-password'>
-            <p className='paragraph-small quote red'>
-              Entre 8 y 24 caracteres {`X`}
+            <p
+              className={`paragraph-small quote ${
+                isCorrectLength ? 'green' : 'red'
+              }`}
+            >
+              Entre 8 y 24 caracteres {isCorrectLength ? '✔' : 'X'}
             </p>
-            <p className='paragraph-small quote red'>Al menos 1 número {`X`}</p>
-            <p className='paragraph-small quote red'>
-              Al menos una mayúscula {`X`}
+            <p
+              className={`paragraph-small quote ${
+                hasANumber ? 'green' : 'red'
+              }`}
+            >
+              Al menos 1 número {hasANumber ? '✔' : 'X'}
+            </p>
+            <p
+              className={`paragraph-small quote ${
+                hasCapitalLetter ? 'green' : 'red'
+              }`}
+            >
+              Al menos una mayúscula {hasCapitalLetter ? '✔' : 'X'}
             </p>
 
             <input
               required
-              className='error'
+              className={` ${isValid ? 'valid' : 'error'}`}
               type='password'
               placeholder='Escribe tu contraseña'
+              onChange={handleMainPasswordInput}
             />
           </div>
           <div className='repeat-password'>
-            <p className='paragraph-small quote red'>
-              Las contraseñas deben coincidir {`X`}
+            <p
+              className={`paragraph-small quote ${
+                passwordsMatch ? 'green' : 'red'
+              }`}
+            >
+              Las contraseñas deben coincidir {passwordsMatch ? '✔' : 'X'}
             </p>
-
             <input
+              className={` ${passwordsMatch ? 'valid' : 'error'}`}
               required
               type='password'
               placeholder='Repite tu contraseña'
+              onChange={handleRepeatPassword}
             />
           </div>
         </div>
         <p className='paragraph-medium'>
-          También puedes crear una puesta que te ayude a recordar tu Contraseña
+          También puedes crear una pista que te ayude a recordar tu Contraseña
           Maestra.
         </p>
         <p className='paragraph-small quote optional'>
-          Crea tu pista para recordar tu contraseña (opcional)
+          Crea tu pista para recordar tu contraseña (opcional){' '}
+          <span className='info'>ⓘ</span>
         </p>
         <input
           className='clue'
+          maxLength='255'
           type='text'
           placeholder='Introduce tu pista'
           autoComplete='off'
+          onChange={(e) => setClueCount(e.target.value.length)}
         />
+        {clueCount}/255
         <div className='SecondView__content--bottom'>
           <Button text='Cancelar' />
-          <Button text='Siguiente' disabled />
+          <Button text='Siguiente' disabled={disabled} clicked={handleSubmit} />
         </div>
       </div>
     </div>
